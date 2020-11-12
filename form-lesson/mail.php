1e77c62598257da2a31eb6b21f581cd5c2cadd2e
tarasglushko <captainpitbull88@gmail.com>
2020-11-12 13:47:58 +0400

    поправки формы отправки

--- form-lesson/mail.php
+++ form-lesson/mail.php
@@ -13,12 +13,12 @@ $text = $_POST['user_text'];
 $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.mail.ru';  																							// Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'captainpitbull88@gmail.com'; // Ваш логин от почты с которой будут отправляться письма
+$mail->Username = 'taras_glushkoo@mail.ru'; // Ваш логин от почты с которой будут отправляться письма
 $mail->Password = 'Nfhfc8274'; // Ваш пароль от почты с которой будут отправляться письма
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
-$mail->Port = 587; // TCP port to connect to / этот порт может отличаться у других провайдеров
+$mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
 
-$mail->setFrom('captainpitbull88@gmail.com'); // от кого будет уходить письмо?
+$mail->setFrom('taras_glushkoo@mail.ru'); // от кого будет уходить письмо?
 $mail->addAddress('taras.glushck@yandex.ru');     // Кому будет уходить письмо 
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
